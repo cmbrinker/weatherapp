b5de64fb02292e4409fa99bbe955b43addd0173d
Chris Brinker <cmbrinker@gmail.com>
2019-03-18 22:43:04 +0100

    city inputs accepted from command line

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const http = require('http');
 const https = require('https');
 
+let city = process.argv.slice(2);
 
-const request = https.get('https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=e29d833be8267adf323f05ed153a18be', (res) => {
-  let body = "";
-  res.on('data', (data) => {
-    body += data.toString();
+function getWeather(city) {
+  const request = https.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=e29d833be8267adf323f05ed153a18be`, (res) => {
+    let body = "";
+    res.on('data', (data) => {
+      body += data.toString();
+    });
+    res.on('end', () => {
+      let currentConditions = JSON.parse(body);
+      console.log(currentConditions.weather[0].description);
+    });
   });
-  res.on('end', () => {
-    let bodyParsed = JSON.parse(body);
-    console.log(bodyParsed);
-  })
-})
+}
+
+getWeather(city);
