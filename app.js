c94ecf22a28b510872385f62842982ea2ffedce5
Chris Brinker <cmbrinker@gmail.com>
2019-03-19 13:59:52 +0100

    error catchers

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const getWeather = require('./getWeather');
 const argvQuery = process.argv.slice(2);
 
-getWeather.get(argvQuery);
+getWeather.get(argvQuery); //select city's weather with: node app.js city
