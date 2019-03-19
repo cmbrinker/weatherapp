48819bc2791a00e33bfa124e3248bea7472675e5
Chris Brinker <cmbrinker@gmail.com>
2019-03-19 09:41:17 +0100

    gets weather from city name with temp and conditions

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 const http = require('http');
 const https = require('https');
+const apiKey = `e29d833be8267adf323f05ed153a18be`;
 
-let city = process.argv.slice(2);
+function printWeather(city, temp, currentConditions) {
+  console.log(`In ${city} the temperature is ${temp} with ${currentConditions}`);
+}
+
+function convertKelvin (kelvin) {
+  return (kelvin - 273.15) * (9/5) + 32;
+}
 
 function getWeather(city) {
-  const request = https.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=e29d833be8267adf323f05ed153a18be`, (res) => {
-    let body = "";
-    res.on('data', (data) => {
-      body += data.toString();
-    });
-    res.on('end', () => {
-      let currentConditions = JSON.parse(body);
-      console.log(currentConditions.weather[0].description);
-    });
+  const request = https.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`, (res) => {
+    if(res.statusCode === 200) {
+      let body = "";
+      res.on('data', (data) => {
+        body += data.toString();
+      });
+      res.on('end', () => {
+        let parsed = JSON.parse(body);
+        let kelvin = parsed.main.temp;
+        let temp = Math.round(convertKelvin(kelvin));
+        let currentConditions = parsed.weather[0].description;
+        return printWeather(`${city}`, `${temp}`, `${currentConditions}`);
+      });
+    } else {
+    console.error(`There was a problem getting the weather, sorry! (Error: ${http.STATUS_CODES[res.statusCode]})`);
+    }
   });
 }
 
-getWeather(city);
+
+getWeather("boston");
